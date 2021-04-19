7d2898c1695b7cc21a5338c4868d5f118d8493ff
soufiane fatih <fatihe.soufiane@gmail.com>
2021-04-19 15:31:10 +0000

    comm

---
+++ project/src/views/help.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="our-help">
+    <h1>help</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ea iure
+      sint sunt, assumenda quisquam nam adipisci explicabo et neque modi fugit
+      perspiciatis, odit voluptates magnam repellendus, ipsa accusantium
+      aliquam?
+    </p>
+  </div>
+</template>
+
