e1c01beb9c70f68f2f2856fb13745f5abb702616
soufiane fatih <fatihe.soufiane@gmail.com>
2021-04-22 22:35:58 +0000

    add

---
+++ portfolio/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+      <Header></Header>
+    
+    
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
---
+++ portfolio/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="header">
+            <!-- <h1>Soufiane Fatih</h1> -->
+     
+    </div>
+</template>
+
+<script>
+export default {
+   name : "header"
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    padding: 0;
+}
+.header{
+    background-image: url(../assets/bg.jpg);
+    background-position: center;
+    background-size: cover;
+    height: 100vh;
+    width: 100%;
+}
+</style>
