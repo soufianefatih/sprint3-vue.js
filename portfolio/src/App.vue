5f9db858eef1f1e5b5f00d4d1cb76007ebeec9f9
soufiane fatih <fatihe.soufiane@gmail.com>
2021-04-23 10:40:12 +0000

    div-section

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
       <Header></Header>
+      <About></About>
+      <Experience></Experience>
+      <Skils></Skils>
+      <Myproject></Myproject>
+      <Footer></Footer>
+    
     
     
   </div>
 </template>
 
+
+
 <script>
+import About from './components/About.vue';
+import Experience from './components/Experience.vue';
 import Header from "./components/Header.vue";
+import Myproject from './components/Myproject.vue';
+
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    About,
+    Experience,
+    Myproject
   }
 };
 </script>
