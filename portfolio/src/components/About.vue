5b8ccfdf81d3bcbc2dadd6da82641d07d2f1fa40
Elhilali-hA <77061440+Elhilali-hA@users.noreply.github.com>
2021-04-23 14:58:56 +0000

    about

--- portfolio/src/components/About.vue
+++ portfolio/src/components/About.vue
@@ -1,7 +1,41 @@
 <template>
     <div class="about">
-        
+        <section class="container-fluid">
+        <div class="column text-center text-align-center">
+        <h1 class="animated swing">ABOUT ME</h1>
+        <p>HOPE TO KNOW YOU AFTER</p>
+        <img src="../assets/photo.jpg" class="rounded-circle" style="width:200px; height:200px;" alt="">
+</div>
+ <div class="row justify-content-center mt-4">
+     <div class="col-7 column">
+         <h6>WHO AM I ?</h6>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis aut repudiandae <br> labore
+             officiis quo praesentium excepturi distinctio, 
+             cumque numquam pariatur <br> nesciunt adipisci nihil corrupti recusandae? Itaque suscipit 
+             molestiae maxime in. <br>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis aut repudiandae <br> labore
+            officiis quo praesentium excepturi distinctio, 
+             cumque numquam pariatur <br> nesciunt adipisci nihil corrupti recusandae? Itaque suscipit 
+             molestiae maxime in.</p>
+     </div>
+     <div class="col-4 column">
+         <h6>HUGO BOLLON</h6>
+         <div>
+        <p> <span>Email:</span>chil3ayba@gmail.com</p>
+        <p> <span>Phone:</span>+123456789</p>
+        <p> <span>City:</span>chambey,france</p>
+        <p> <span>Languages:</span>french,english</p>
+         </div>
+         <div class="row  col-12 ">
+         <i class="fab fa-facebook-square"></i>
+         <i class="fab fa-linkedin"></i>
+         <i class="fab fa-instagram-square"></i>
+         <i class="fab fa-github-square"></i>
+         </div>
+     </div>
+ </div> 
 
+ </section>
     </div>
 </template>
 
