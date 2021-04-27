73fa2b8030208b4a7682280d5e19f673011be0dd
soufiane fatih <fatihe.soufiane@gmail.com>
2021-04-27 15:05:18 +0000

    vue

--- portfolio/src/components/Experience.vue
+++ portfolio/src/components/Experience.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div class="container-fluid bg-dark soso">
       <div class="mt-5" data-aos="fade-down"
      data-aos-easing="linear"
@@ -52,56 +51,6 @@
             </div>
         </div>
      </div>
-=======
-        <div class="about bg-secondary vh-90 text-white">
-        <section class="container-fluid p-5">
-        <div class="column text-center text-align-center">
-        <h1 class="animated swing ">EXPERIENCE</h1>
-        <p>PROFESSIONAL AND ACADEMIC</p>
-        <hr class="solid col-1 bg-white">
-        
-</div>
- <div class="row mt-4 text-white">
-     <div class="col-sm-6 text-right column">
-         <h6>EDUCATION</h6>
-        <div>
-        <p> 01.2020 - 05.2020 <br>
-        <span> Urlabo LAET Project</span> <br>
-        <span> Lorem, ipsum dolor sit amet consectetur adipisicing elit.fugit qui aut corporis.</span></p>
-         </div>
-         <div>
-        <p> 01.2020 - 05.2020 <br>
-        <span> Urlabo LAET Project</span> <br>
-        <span> Lorem, ipsum dolor sit amet consectetur adipisicing elit.fugit qui aut corporis.</span></p>
-         </div>
-         <div>
-        <p> 01.2020 - 05.2020 <br>
-        <span> Urlabo LAET Project</span> <br>
-        <span> Lorem, ipsum dolor sit amet consectetur adipisicing elit.fugit qui aut corporis.</span></p>
-         </div>
-     </div>
-     <div class="col-sm-6 column border-left">
-         <h6>PROFESSIONAL</h6>
-         <div>
-        <p> 01.2020 - 05.2020 <br>
-        <span> Urlabo LAET Project</span> <br>
-        <span> Lorem, ipsum dolor sit amet consectetur adipisicing elit.fugit qui aut corporis.</span></p>
-         </div>
-         <div>
-        <p> 01.2020 - 05.2020 <br>
-        <span> Urlabo LAET Project</span> <br>
-        <span> Lorem, ipsum dolor sit amet consectetur adipisicing elit.fugit qui aut corporis.</span></p>
-         </div>
-         <div>
-        <p> 01.2020 - 05.2020 <br>
-        <span> Urlabo LAET Project</span> <br>
-        <span> Lorem, ipsum dolor sit amet consectetur adipisicing elit.fugit qui aut corporis.</span></p>
-         </div>
-         
-     </div>
-  </div>
-</section>
->>>>>>> dd0c3c65b90bb7d22e380033cd488d72862d9ac8
     </div>
 </template>
 <script>
